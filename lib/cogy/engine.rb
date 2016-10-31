9a5c9ed3dedd803e5c1c55d821211528c5c4152c
Agis Anastasopoulos <agis.anast@gmail.com>
2016-10-31 11:30:09 +0200

    Raise if executable paths isn't set

--- lib/cogy/engine.rb
+++ lib/cogy/engine.rb
@@ -6,6 +6,11 @@ module Cogy
       if Cogy.executable_path.nil?
         raise "You must set the :executable_path setting"
       end
+
+      Cogy.command_load_paths.each do |path|
+        files = Dir[Rails.root.join(path,"*.rb")]
+        files.each { |f| require_relative(f) }
+      end
     end
   end
 end
