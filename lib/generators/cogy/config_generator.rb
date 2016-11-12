cd3ac72387a4e805457739828ff8d4de8c40a826
Agis Anastasopoulos <agis.anast@gmail.com>
2016-11-12 18:02:58 +0200

    Add config generator
    Closes #4

---
+++ lib/generators/cogy/config_generator.rb
@@ -0,0 +1,15 @@
+module Cogy
+  module Generators
+    class ConfigGenerator < Rails::Generators::Base
+      source_root File.expand_path("../templates", __FILE__)
+
+      desc <<DESC
+Description:
+    Copies Cogy configuration file to your application's initializer directory.
+DESC
+      def copy_config_file
+        template 'cogy_config.rb', 'config/initializers/cogy.rb'
+      end
+    end
+  end
+end
