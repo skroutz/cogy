skroutz/cogy

commit 84896ef3d4d15fc82cc86e2b71f8c5cdd72b1e11

Ship COGY_BACKEND env. var to cogy-bundle
Closes #37.

lib/cogy/capistrano/cogy.rake
@@ -6,11 +6,11 @@ namespace :cogy do
     require "json"
 
     trigger_url = fetch(:cogy_release_trigger_url)
-    cogy_endpoint = fetch(:cogy_endpoint)
+    cogy_endpoint = Cogy.cogy_endpoint
 
     begin
       raise ":cogy_release_trigger_url must be set" if trigger_url.nil?
-      raise ":cogy_endpoint must be set" if cogy_endpoint.nil?
+      raise "Cogy.cogy_endpoint must be set" if cogy_endpoint.nil?
 
       Timeout.timeout(fetch(:cogy_trigger_timeout) || 7) do
         url = URI(trigger_url)

lib/cogy.rb
@@ -45,6 +45,11 @@ module Cogy
   @@command_load_paths = ["cogy"]
   mattr_accessor :command_load_paths
 
+  # The endpoint where the Cogy engine is reachable at.
+  # For example "http://www.example.com/cogy"
+  @@cogy_endpoint = nil
+  mattr_accessor :cogy_endpoint
+
   # Initializes a new {Command} and registers it. All the options passed are
   # used when generating the bundle config in {Cogy.bundle_config}.
   #
@@ -128,6 +133,12 @@ module Cogy
 
       if cmd.examples
         config["commands"][name]["examples"] = cmd.examples
+      end
+
+      if Cogy.cogy_endpoint
+        config["commands"][name]["env_vars"] = {
+          "COGY_BACKEND" => Cogy.cogy_endpoint
+        }
       end
     end
 

lib/generators/cogy/templates/cogy_config.rb
@@ -8,4 +8,5 @@ Cogy.configure do |config|
   #}
   #
   #config.command_load_paths = ["cogy"]
+  #config.cogy_endpoint = "http://www.example.com/cogy"
 end

test/dummy/config/initializers/cogy.rb
@@ -7,6 +7,7 @@ Cogy.configure do |c|
   }
 
   c.command_load_paths = ["cogy"]
+  c.cogy_endpoint = "http://www.example.com/cogy"
 
   c.helper(:foo) { env["cog_foo"] }
   c.helper(:bar) { |text| text.titleize }

test/integration/inventory_test.rb
@@ -32,7 +32,8 @@ module Cogy
         expected = {
           "executable" => "/bin/no",
           "description" => "Print a foo",
-          "rules" => ["allow"]
+          "rules" => ["allow"],
+          "env_vars" => { "COGY_BACKEND" => "http://www.example.com/cogy" }
         }
 
         assert_equal expected, inv["commands"]["say_foo"]
@@ -51,8 +52,6 @@ module Cogy
     end
 
     def test_template
-      get "/cogy/inventory"
-
       expected = <<TEMPLATE.strip
 **foo:** ~$results[0].name~
 

commit 9f5bfb47aa5dc82390472693fab5822e3dbcb7fb

Fix Capistrano integration regression
84896ef3 broke the capistrano integration, since we were trying to
access Cogy as if it was loaded by Rails, but it weren't.

Unfortunately, we can't read the already provided `Cogy.cogy_endpoint`
inside the capistrano task without some sort of magic. For the time
being, we'll go with the old behavior, which means we have to still
define the cogy endpoint in the Capistrano task too, even though we
already provide it in the Rails configuration.

lib/cogy/capistrano/cogy.rake
@@ -6,11 +6,11 @@ namespace :cogy do
     require "json"
 
     trigger_url = fetch(:cogy_release_trigger_url)
-    cogy_endpoint = Cogy.cogy_endpoint
+    cogy_endpoint = fetch(:cogy_endpoint)
 
     begin
       raise ":cogy_release_trigger_url must be set" if trigger_url.nil?
-      raise "Cogy.cogy_endpoint must be set" if cogy_endpoint.nil?
+      raise ":cogy_endpoint must be set" if cogy_endpoint.nil?
 
       Timeout.timeout(fetch(:cogy_trigger_timeout) || 7) do
         url = URI(trigger_url)